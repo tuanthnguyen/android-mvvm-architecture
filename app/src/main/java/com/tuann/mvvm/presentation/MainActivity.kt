643cff1be148cbf193a152573c4b16bc04596e29
Tuan Nguyen <nttuan2411@gmail.com>
2018-11-14 10:29:36 +0700

    test danger bot

--- app/src/main/java/com/tuann/mvvm/presentation/MainActivity.kt
+++ app/src/main/java/com/tuann/mvvm/presentation/MainActivity.kt
@@ -20,7 +20,8 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
+        if (savedInstanceState == null)
+        {
             navigator.navigateToImages()
         }
     }
